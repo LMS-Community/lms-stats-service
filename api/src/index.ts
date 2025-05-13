8657b2bbf9771adc6defb22a43013d36b846da2f
Michael Herger <michael@herger.net>
2025-05-13 19:40:04 +0200

    Remove `/time` and `/geoip` endpoints which now are on the API service.

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,6 @@ import { Hono } from 'hono/tiny'
 import packages from './packages'
 import instance from './instance'
 import stats from './stats'
-import time from './time'
 
 const app = new Hono()
 
@@ -14,24 +13,7 @@ app.get('/', async c => {
 app.route('/api/instance', instance)
 app.route('/api/packages', packages)
 app.route('/api/stats', stats)
-app.route('/api/time', time)
 
 app.get('/health', async c => c.json({ status: 'ok' }))
-
-app.get('/api/geoip', async c => {
-    return c.json({
-        city: c.req.header('cf-ipcity'),
-        country: c.req.header('cf-ipcountry'),
-        continent: c.req.header('cf-ipcontinent'),
-        latitude: c.req.header('cf-iplatitude'),
-        longitude: c.req.header('cf-iplongitude'),
-        region: c.req.header('cf-region'),
-        regionCode: c.req.header('cf-region-code'),
-        metroCode: c.req.header('cf-metro-code'),
-        postalCode: c.req.header('cf-postal-code'),
-        timezone: c.req.header('cf-timezone'),
-        ip: c.req.header('cf-connecting-ip'),
-    })
-})
 
 export default app
